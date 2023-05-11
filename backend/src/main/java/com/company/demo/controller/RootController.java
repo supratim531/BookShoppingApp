23584cc4a71727289e42fe8b328afd4007a9e7ae
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-05-11 11:07:38 +0530

    supratim531: Modified RootController

--- backend/src/main/java/com/company/demo/controller/RootController.java
+++ backend/src/main/java/com/company/demo/controller/RootController.java
@@ -8,7 +8,7 @@ public class RootController {
 
 	@GetMapping("/")
 	public String intro() {
-		return "<!doctype html>\r\n" + "<html lang=\"en\">\r\n" + "\r\n" + "<head>\r\n"
+		return "<!DOCTYPE html>\r\n" + "<html lang=\"en\">\r\n" + "\r\n" + "<head>\r\n"
 				+ "  <!-- Required meta tags -->\r\n" + "  <meta charset=\"utf-8\">\r\n"
 				+ "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + "\r\n"
 				+ "  <!-- Bootstrap CSS and JS -->\r\n"
@@ -17,17 +17,18 @@ public class RootController {
 				+ "  <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\"\r\n"
 				+ "    integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\"\r\n"
 				+ "    crossorigin=\"anonymous\"></script>\r\n" + "\r\n"
-				+ "  <title>IEM | Book Shopping Application</title>\r\n" + "</head>\r\n" + "\r\n" + "<body>\r\n"
-				+ "  <div class=\"container my-5\">\r\n" + "    <h1>Book Application (Backend)</h1>\r\n"
-				+ "    <hr class=\"text-danger\">\r\n"
+				+ "  <title>IEM | Book Shopping Application</title>\r\n" + "\r\n" + "  <style>\r\n"
+				+ "    @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap');\r\n"
+				+ "  </style>\r\n" + "</head>\r\n" + "\r\n" + "<body>\r\n" + "  <div class=\"container my-5\">\r\n"
+				+ "    <h1>Book Application (Backend)</h1>\r\n" + "    <hr class=\"text-danger\">\r\n"
 				+ "    <h3 style=\"color: rgb(228, 101, 51);\">All Crew Members</h3>\r\n" + "    <big>\r\n"
 				+ "      <ul style=\"font-size: 20px; font-family: fira code;\">\r\n" + "        <li>Ankita De</li>\r\n"
-				+ "        <li>Arpan Ghosh</li>\r\n" + "        <li>Dipika Mondol</li>\r\n"
+				+ "        <li>Arpan Ghosh</li>\r\n" + "        <li>Dipika Mondal</li>\r\n"
 				+ "        <li>Ishani Dhar</li>\r\n" + "      </ul>\r\n" + "    </big>\r\n" + "    <b>\r\n"
-				+ "      <big style=\"font-size: 20px; font-family: fira code;\">Test all REST-API <a\r\n"
-				+ "          href=\"https://localhost:8888/swagger-ui/index.html\" style=\"text-decoration: none;\"\r\n"
-				+ "          target=\"_blank\">here</a> (N.B.: Frontend part is coming soon...)</big>\r\n"
-				+ "    </b>\r\n" + "  </div>\r\n" + "</body>\r\n" + "\r\n" + "</html>";
+				+ "      <big style=\"font-family: 'Fira Code', monospace;\">Test all REST-API <a\r\n"
+				+ "          href=\"https://localhost:8888/swagger-ui/index.html\" style=\"text-decoration: none;\" target=\"_blank\">here</a>\r\n"
+				+ "        (N.B.: Frontend part is ready)</big>\r\n" + "    </b>\r\n" + "  </div>\r\n" + "</body>\r\n"
+				+ "\r\n" + "</html>";
 	}
 
 }
